29cd4cc0d70581b52e1a28954763f05fbb17c6d1
jose sabater <jose.sabater.iglesias@gmail.com>
2023-05-12 14:57:35 +0200

    add create and update pages

--- test_sdk.py
+++ test_sdk.py
@@ -3,8 +3,81 @@ import os
 from notion_client import Client
 
 TOKEN = settings.token
-
 notion = Client(auth=TOKEN)
-PAGE_ID = "e5a81e31-fb9c-421b-9288-2eeb57de01d8"
 
-print(notion.databases.retrieve(database_id=PAGE_ID))
+DATABASE_ID = settings.database_id
+
+
+# Retrieve database info
+# databases = notion.databases.retrieve(database_id=DATABASE_ID)
+# print(databases["last_edited_time"])
+
+# -------------------------------------------------------------------------------
+# create
+# notion.pages.create(
+#     parent={"database_id": DATABASE_ID},
+#     properties={
+#         "Title": {
+#             "title": [
+#                 {
+#                     "text": {"content": "This is the first test"},
+#                     "plain_text": "second text",
+#                 }
+#             ]
+#         },
+#     },
+# )
+
+notion.pages.create(
+    parent={"database_id": DATABASE_ID},
+    properties={
+        "Title": {
+            "title": [
+                {
+                    "text": {"content": "This is the first test"},
+                    "plain_text": "second text",
+                },
+            ]
+        }
+    },
+    children=[
+        {
+            "object": "block",
+            "type": "heading_2",
+            "heading_2": {
+                "rich_text": [{"type": "text", "text": {"content": "Heading 2"}}],
+            },
+        },
+        {
+            "object": "block",
+            "type": "paragraph",
+            "paragraph": {
+                "rich_text": [{"type": "text", "text": {"content": "## Paragraph"}}]
+            },
+        },
+    ],
+)
+
+
+# -------------------------------------------------------------------------------
+# Retrieve pages and update
+# pages = notion.databases.query(database_id=DATABASE_ID)
+# for page in pages["results"]:
+#     print(page["properties"]["Title"]["title"][0]["plain_text"])
+#     if (
+#         page["properties"]["Title"]["title"][0]["plain_text"]
+#         == "This is the first test"
+#     ):
+#         notion.pages.update(
+#             page_id=page["id"],
+#             properties={
+#                 "Title": {
+#                     "title": [
+#                         {
+#                             "text": {"content": "This is the second test"},
+#                             "plain_text": "second text",
+#                         }
+#                     ]
+#                 },
+#             },
+#         )
